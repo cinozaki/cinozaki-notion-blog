deb0a2d51def5d3fc82666c2fa713e3a724b5aac
cinozaki <cinozaki@gmail.com>
2021-01-31 11:16:55 +0900

    footer link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
 import ExtLink from './ext-link'
 
 export default () => (
   <>
     <footer>
       <span>
-        blog.cinozaki.com
+
+      <Link
+        href="/"
+      >
+        <a>&copy; blog.cinozaki.com</a>
+      </Link>
       </span>
     </footer>
   </>
